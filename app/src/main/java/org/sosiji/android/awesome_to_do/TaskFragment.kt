a4b3639a0a3a7b67dfd1457bb5974690c1f17ea8
유지상 <jidbyoo@gmail.com>
2021-12-01 23:07:18 +0900

    Update: #5 - Fragment.onCreate 오버라이드

--- app/src/main/java/org/sosiji/android/awesome_to_do/TaskFragment.kt
+++ app/src/main/java/org/sosiji/android/awesome_to_do/TaskFragment.kt
@@ -1,6 +1,14 @@
 package org.sosiji.android.awesome_to_do
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class TaskFragment : Fragment() {
+
+    private lateinit var task: Task
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        task = Task()
+    }
 }
