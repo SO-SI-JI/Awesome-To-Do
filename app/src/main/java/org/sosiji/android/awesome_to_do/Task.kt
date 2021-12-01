3f1506c36dbe6a335c4b550bb61d948edea52f60
유지상 <jidbyoo@gmail.com>
2021-12-01 23:04:30 +0900

    Update: #5 - Task 클래스 속성 추가

--- app/src/main/java/org/sosiji/android/awesome_to_do/Task.kt
+++ app/src/main/java/org/sosiji/android/awesome_to_do/Task.kt
@@ -1,4 +1,8 @@
 package org.sosiji.android.awesome_to_do
 
-class Task {
-}
+import java.util.*
+
+data class Task(val id: UUID = UUID.randomUUID(),
+                var title: String = "",
+                var date: Date = Date(),
+                var isDone: Boolean = false)
