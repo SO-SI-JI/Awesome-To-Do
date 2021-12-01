189512bc4a70114ea90a2e550c19d52dee45dfcd
유지상 <jidbyoo@gmail.com>
2021-12-01 23:10:14 +0900

    Update: #5 - 프래그먼트 뷰 명시적 인플레이트

--- app/src/main/java/org/sosiji/android/awesome_to_do/TaskFragment.kt
+++ app/src/main/java/org/sosiji/android/awesome_to_do/TaskFragment.kt
@@ -1,6 +1,9 @@
 package org.sosiji.android.awesome_to_do
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class TaskFragment : Fragment() {
@@ -10,5 +13,10 @@ class TaskFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         task = Task()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?{
+        val view = inflater.inflate(R.layout.fragment_task, container, false)
+        return view
     }
 }
